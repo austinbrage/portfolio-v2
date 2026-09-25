a2b6b39b215f41c562897def3ebeb1aa17b9b9cf
Austin Brage <agustinbrage19@gmail.com>
2026-09-25 14:01:53 -0300

    feat: keep current page when toggling lang on navbar dropdown

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -13,5 +13,11 @@ document.addEventListener("alpine:init", () => {
       localStorage.setItem("theme", this.theme);
       document.documentElement.classList.toggle("dark", this.theme === "dark");
     },
+
+    localizedPath(code) {
+      var parts = window.location.pathname.split("/");
+      parts[1] = code;
+      return parts.join("/") + window.location.search + window.location.hash;
+    },
   }));
 });
